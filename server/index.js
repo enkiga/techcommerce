f3a74ff822e24ba54fd624ff94bba70775c55574
Enoch Garoli <eenkiga@gmail.com>
2025-04-12 13:05:12 +0300

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
-require('dotenv').config(); 
+require("dotenv").config(); //Connecting server to .env file
+// Importing requirement
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Calling and setting up express
 const app = express();
-
-const port = process.env.PORT || 8000
-
 app.use(express.json());
+
+// Assigning port value
+const port = process.env.PORT || 8000;
+
+// Connecting to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Server de be running" });
 });
 
+// Listening to the connection
 app.listen(port, () => {
   console.log("listening...");
 });
